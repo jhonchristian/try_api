57ae16f8203853b80c247e980ba003c5e16003c3
jeysee <ianparucha04@gmail.com>
2019-02-07 12:20:17 +0800

    Class Added: Database Class

---
+++ config/Database.php
@@ -0,0 +1,32 @@
+<?php 
+    class Database
+    {
+        // DB params
+        private $host     = 'localhost';
+        private $db_name  = 'try_api';
+        private $username = 'root';
+        private $password = '';
+        private $conn     = null;
+
+        // DB Connect
+        public function connect()
+        {
+            $this->conn = null;
+            try 
+            {
+                $database = 'mysql:host='.$this->host
+                          . ';dbname='   .$this->db_name
+                          . $this->username
+                          . $this->password;
+                $this->conn = new PDO($database);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch ( PDOException $e)
+            {
+                echo 'Connection Error: '.$e->getMessage();
+            }
+
+            return $this->conn;
+        }
+    }
+?>
